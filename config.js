894639a84337079f347fe5646517c6d4c514d9b6
SannzBotz <102431993+SannzBotz@users.noreply.github.com>
2022-03-27 09:46:46 +0700

    Config.js

--- config.js
+++ config.js
@@ -18,9 +18,9 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','6281382420312','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
+global.owner = ['6281258657607','6281382420312','6288292024190']
+global.premium = ['6281258657607']
+global.packname = 'SanzGans'
 global.author = 'WhatsApp Bot'
 global.sessionName = 'hisoka'
 global.prefa = ['','!','.','🐦','🐤','🗿']
